8b6467c2d9220f66762e85f6a4416eb028d54a6b
Shreyansh Mishra <shreyanshmishracr7@gmail.com>
2024-09-02 01:36:27 +0530

    styled header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,45 @@
-
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <div>Header says hello! </div>
-  )
+    <div className="flex justify-center items-center p-4">
+      <div className="absolute bg-[#18B2DE] h-[150px] w-[800px] m-auto -top-[120px] filter blur-[90px] -z-10"></div>
+      <div className="flex justify-center items-center gap-5 text-purpleGrey font-medium transition-all duration-300">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "text-white" : "hover:text-white"
+          }
+        >
+          Signup
+        </NavLink>
+        <NavLink
+          to="/podcasts"
+          className={({ isActive }) =>
+            isActive ? "text-white" : "hover:text-white"
+          }
+        >
+          Podcasts
+        </NavLink>
+        <NavLink
+          to="/start-podcast"
+          className={({ isActive }) =>
+            isActive ? "text-white" : "hover:text-white"
+          }
+        >
+          Start a Podcast
+        </NavLink>
+        <NavLink
+          to="/profile"
+          className={({ isActive }) =>
+            isActive ? "text-white" : "hover:text-white"
+          }
+        >
+          Profile
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purpleGrey: 'var(--purple-grey)',
+        white: 'var(--white)',
+      },
+    },
   },
   plugins: [],
 }
